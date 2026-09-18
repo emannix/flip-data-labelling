e5a5011cbfb1492a7f542e266de67e3d5ed6d298
Evelyn Mannix <mannix.evelyn@gmail.com>
2026-09-18 13:43:20 +1000

    added copyupdatasets

--- copyupdatasets.bash
+++ copyupdatasets.bash
@@ -9,3 +9,10 @@ sudo mount -t cifs -ouser=mannixe,domain=unimelb,uid=$UID,gid=$(id -g) \
 # rsync -ahP --info=progress2 original_master_2026_09_04.tar.gz mnt/farm_classification/data/original_new_2026/
 # rsync -ahP --info=progress2 original_master_2026_09_04.tar.gz emannix@spartan.hpc.unimelb.edu.au:/data/cephfs/punim1019/FLIP-phase-3/data/
 
+
+# SIZE=$(du -sb original_master_2026_09_18 | awk '{print $1}')
+# time tar -cf - original_master_2026_09_18 | pv -s "$SIZE" | pigz -0 > original_master_2026_09_18.tar.gz
+# # copy to mediaflux
+# rsync -ahP --info=progress2 original_master_2026_09_18.tar.gz mnt/farm_classification/data/original_new_2026/
+# rsync -ahP --info=progress2 original_master_2026_09_18.tar.gz emannix@spartan.hpc.unimelb.edu.au:/data/cephfs/punim1019/FLIP-phase-3/data/
+
